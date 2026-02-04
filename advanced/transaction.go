fdc54985e1b3b536ca8823b2e27f818ef458f008
Liang Zhuang <bzhuangliang@gmail.com>
2026-02-04 11:38:09 -0800

    adds example for finer transaction control

--- advanced/transaction.go
+++ advanced/transaction.go
@@ -20,59 +20,141 @@ func TransactionTest() {
 	dsn := "db/transaction.db"
 	db := setup(dsn)
 
-	AutoTransactionTest(db)
+	// autoTransactionTest(db)
+	manualTransactionTest(db)
 }
 
-func AutoTransactionTest(db *gorm.DB) {
-	err := db.Transaction(func(tx *gorm.DB) error {
-		// 1. Find the user
-		var u User
-		if err := tx.Preload("Orders").Preload("Orders.Items").Preload("Orders.Items.Product").First(&u, 1).Error; err != nil {
-			fmt.Println("user not found")
-			return err // roll back
-		}
-
-		before, _ := json.MarshalIndent(u, "", "  ")
-		fmt.Println(string(before))
-
-		// 2. Build the order
+func autoTransaction(db *gorm.DB, u *User) error {
+	return db.Transaction(func(tx *gorm.DB) error {
+		// build the order
 		items := []OrderItem{
 			{ProductID: 1, Quantity: 1, Price: 999.00},
 			{ProductID: 2, Quantity: 1, Price: 249.00},
 		}
 		order := Order{
-			OrderNumber: "ORD-3001", // Change to ORD-1001 to trigger roll back
-			UserID:      u.ID,       // FK to users table
+			OrderNumber: "ORD-3001", // change to ORD-1001 to trigger roll back
+			UserID:      u.ID,       // capture parameters in closure, and FK to users table
 			TotalPrice:  items[0].Price*float64(items[0].Quantity) + items[1].Price*float64(items[1].Quantity),
 			Status:      "created",
 			Items:       items,
 		}
+
 		if err := tx.Create(&order).Error; err != nil {
 			fmt.Println("fails to create order", err)
 			return err // roll back
 		}
 
-		// commit
 		return nil
 	})
+}
 
-	if err != nil {
-		fmt.Println(err)
-	}
-
+func autoTransactionTest(db *gorm.DB) {
+	// find the user
+	userID := uint(1)
 	var u User
-	if err := db.Preload("Orders").Preload("Orders.Items").Preload("Orders.Items.Product").Find(&u, 1).Error; err != nil {
+	if err := db.
+		Preload("Orders").
+		Preload("Orders.Items").
+		Preload("Orders.Items.Product").
+		First(&u, userID).Error; err != nil {
 		panic(err)
 	}
 
-	after, _ := json.MarshalIndent(u, "", "  ")
+	// take a snapshot before the tansaction
+	before, _ := json.MarshalIndent(u, "", "  ")
+	fmt.Println(string(before))
+
+	// start the transaction
+	if err := autoTransaction(db, &u); err != nil {
+		fmt.Println("fails to create order", err)
+	}
+
+	// reload to verify
+	var refreshed User
+	if err := db.
+		Preload("Orders").
+		Preload("Orders.Items").
+		Preload("Orders.Items.Product").
+		First(&refreshed, userID).Error; err != nil {
+		panic(err)
+	}
+
+	after, _ := json.MarshalIndent(&refreshed, "", "  ")
 	fmt.Println(string(after))
 }
 
-func ManualTransactionTest(db *gorm.DB) {
+func manualTransactionTest(db *gorm.DB) {
+	// start a transaction
+	tx := db.Begin()
+	if tx.Error != nil {
+		panic(tx.Error)
+	}
+
+	// protect against panics
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	// find the user
+	userID := uint(1)
+	var u User
+	if err := db.
+		Preload("Orders").
+		Preload("Orders.Items").
+		Preload("Orders.Items.Product").
+		First(&u, userID).Error; err != nil {
+		fmt.Println("user not found", err)
+		tx.Rollback()
+	}
+
+	// take a snapshot before change
+	before, _ := json.MarshalIndent(u, "", "  ")
+	fmt.Println(string(before))
+
+	// build the order
+	items := []OrderItem{
+		{ProductID: 1, Quantity: 1, Price: 999.00},
+		{ProductID: 2, Quantity: 1, Price: 249.00},
+	}
+	order := Order{
+		OrderNumber: "ORD-3001", // change to ORD-1001 to trigger roll back
+		UserID:      u.ID,       // capture parameters in closure, and FK to users table
+		TotalPrice:  items[0].Price*float64(items[0].Quantity) + items[1].Price*float64(items[1].Quantity),
+		Status:      "created",
+		Items:       items,
+	}
+
+	if err := tx.Create(&order).Error; err != nil {
+		fmt.Println("fails to create order", err)
+		tx.Rollback()
+	}
+
+	// commit the transaction
+	if err := tx.Commit().Error; err != nil {
+		fmt.Println("fails to commit", err)
+		tx.Rollback()
+	}
+
+	// reload to verify
+	var refreshed User
+	if err := db.
+		Preload("Orders").
+		Preload("Orders.Items").
+		Preload("Orders.Items.Product").
+		First(&refreshed, userID).Error; err != nil {
+		panic(err)
+	}
+
+	after, _ := json.MarshalIndent(&refreshed, "", "  ")
+	fmt.Println(string(after))
+}
+
+func savePointTest(db *gorm.DB) {
 
 }
 
-func StopPointTest(db *gorm.DB) {
+func nestedTransactionsTest(db *gorm.DB) {
 
 }
