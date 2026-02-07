7f46d85d24a8364015b5a009084b72809fc273bd
Liang Zhuang <bzhuangliang@gmail.com>
2026-02-06 22:54:43 -0800

    creates blog models

---
+++ project/models.go
@@ -0,0 +1,33 @@
+package project
+
+import "time"
+
+type User struct {
+	ID    uint   `gorm:"primaryKey"`
+	Name  string `gorm:"size:64;not null"`
+	Email string `gorm:"size:64;uniqueIndex;not null"`
+	Posts []Post
+}
+
+type Post struct {
+	ID        uint   `gorm:"primaryKey"`
+	Subject   string `gorm:"size:255;not null"`
+	Content   string `gorm:"not null"`
+	UserID    uint   `gorm:"index"` // FK
+	Tags      []Tag  `gorm:"many2many:post_tags"`
+	Comments  []Comment
+	CreatedAt time.Time `gorm:"autoCreateAt"`
+}
+
+type Tag struct {
+	ID    uint   `gorm:"primaryKey"`
+	Name  string `gorm:"size:64;uniqueIndex;not null"`
+	Posts []Post `gorm:"many2many:post_tags"`
+}
+
+type Comment struct {
+	ID      uint   `gorm:"primaryKey"`
+	PostID  uint   `gorm:"index"` // FK
+	Content string `gorm:"not null"`
+	UserID  uint   `gorm:"index"` // FK
+}
